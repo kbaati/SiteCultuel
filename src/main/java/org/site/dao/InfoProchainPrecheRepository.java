bd0c191067a0fe49d01ee7e41b453b8b8693dad3
baati <baati@192.168.1.29>
2018-04-15 14:45:47 +0200

    add dao infopreche

---
+++ src/main/java/org/site/dao/InfoProchainPrecheRepository.java
@@ -0,0 +1,10 @@
+package org.site.dao;
+
+import java.util.Date;
+
+import org.site.entities.InfoProchainPreche;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InfoProchainPrecheRepository extends JpaRepository<InfoProchainPreche, Date> {
+
+}
